d711f99060c780830af800f1e6d48dc8c077b533
0xx7 <securityx3@gmail.com>
2020-04-17 16:45:53 +0400

    Delete sqli_database.py

--- sqli_database.py
+++
@@ -1,9 +0,0 @@
-# very simle class to represent DB model
-# Name, tables, no more
-class Database:
-    def __init__(self, name):
-        self.db_name = name
-        self.db_tables = []
-
-    def set_db_tables(self, table):
-        self.db_tables = table
